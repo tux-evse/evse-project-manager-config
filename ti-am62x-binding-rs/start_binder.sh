cfccd2d6820c330d938b37ba93ed5b8a861194e1
Ronan Le Martret <ronan.lemartret@iot.bzh>
2024-01-04 11:44:51 +0100

    Update ti-am62x-binding-rs
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- ti-am62x-binding-rs/start_binder.sh
+++ ti-am62x-binding-rs/start_binder.sh
@@ -3,5 +3,5 @@
 /usr/bin/afb-binder --config /usr/redpesk/ti-am62x-binding-rs-test/etc/binder-test.json \
                     --config /usr/redpesk/ti-am62x-binding-rs-test/etc/binding-ti-am62x.json \
                     --config /usr/redpesk/ti-am62x-binding-rs-test/etc/binding-i2c.json \
-                    --ws-server sd:ti-am62x \
-                    --binding /run/user/$UID/apis/binding/ti-am62x-binding-rs
+                    --port 1324 \
+                    -v
