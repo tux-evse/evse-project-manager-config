56a21ea1ace75cdb95ee6ef4ce525ecdfd1989f3
Ronan Le Martret <ronan.lemartret@iot.bzh>
2024-01-05 14:01:40 +0100

    Fix energy-binding-rs
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- energy-binding-rs/start_binder.sh
+++ energy-binding-rs/start_binder.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 cynagora-admin set '' 'HELLO' '' '*' yes
 
-/usr/bin/afb-binder --config /usr/redpesk/energy-binding-rs-test/etc/binding-test.json \
+/usr/bin/afb-binder --config /usr/redpesk/energy-binding-rs-test/etc/binder-test.json \
                     --config /usr/redpesk/energy-binding-rs-test/etc/binding-energy.json \
                     --config /usr/redpesk/energy-binding-rs-test/etc/binding-linky.json \
                     --config /usr/redpesk/energy-binding-rs-test/etc/binding-modbus.json \
