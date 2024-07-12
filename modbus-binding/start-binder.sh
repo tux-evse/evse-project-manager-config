bf3560147e21bafa271fe0012fd39ae022cb8175
Ronan Le Martret <ronan.lemartret@iot.bzh>
2024-07-12 11:35:45 +0200

    Update to os batz 2.0

---
+++ modbus-binding/start-binder.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+cynagora-admin set '' 'HELLO' '' '*' yes
+
+/usr/bin/afb-binder --config /usr/redpesk/modbus-binding/test/etc/binder-test.json \
+                    --config /usr/redpesk/modbus-binding/test/etc/binding-modbus.json \
+                    -v \
+                    --tracereq=all \
+                    $*
+                    
