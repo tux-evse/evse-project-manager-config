efc34ffaf4fe59225ee92889a40a27f4dd01a23f
Hugo Mercier <hugo.mercier@iot.bzh>
2025-01-23 13:31:14 +0100

    Set rslac config from command line
    This prevents rslac from waiting for its configuration.
    
    Signed-off-by: Hugo Mercier <hugo.mercier@iot.bzh>

--- josev-rslac/start.sh
+++ josev-rslac/start.sh
@@ -1,2 +1,4 @@
-#!/usr/bin/env bash
-LOG_LEVEL=DEBUG /usr/josev/rslac/slac_service
+#!/usr/bin/bash
+jq '.set."libafb_josev.so".cs_parameters' /usr/redpesk/evse-charging-manager-binder/etc/binding-josev-ac.json > /tmp/cs_config.json
+jq '.set."libafb_josev.so".cs_status_and_limits' /usr/redpesk/evse-charging-manager-binder/etc/binding-josev-ac.json > /tmp/cs_limits.json
+CS_CONFIG_FILE=/tmp/cs_config.json CS_LIMITS_FILE=/tmp/cs_limits.json LOG_LEVEL=DEBUG /usr/josev/rslac/slac_service
